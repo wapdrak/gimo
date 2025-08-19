0ebf3a0bc59535975dfa555e413292d116642d88
Petr Adamíra <wapdrak@gmail.com>
2025-08-19 22:58:58 +0300

    Add files via upload

---
+++ stamp/q26/b/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="he" dir="auto">
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>חותמת Q26 דיו שחור</title>
+		
+<?php include '../../../head/index.php';?>
+
+</head>
+
+<body>
+
+      <div class="table border4px bordergroove borderblack radius25px margin1 padding1">
+	  
+             <div class="tcaption fweightb center">
+			 <h4 class="fweightb">
+			 פרטי חותמת
+			 </h4>
+			 </div>
+			 <div class="trow">
+                <div class="tcell padding1 width15px">✔</div>
+                <div class="tcell padding1 fweightb">גודל: </div>
+                <div class="tcell padding1">83*16 מ"מ</div>
+             </div>
+            <div class="trow">
+                <div class="tcell padding1 width15px">✔</div>
+                <div class="tcell padding1 fweightb">מלל: </div>
+                <div class="tcell padding1">עד 4 שורות</div>
+            </div>
+            <div class="trow">
+                <div class="tcell padding1 width15px">✔</div>
+                <div class="tcell padding1 fweightb">צבע דיו:</div>
+                <div class="tcell padding1">שחור</div>
+           </div>
+            <div class="trow">
+                <div class="tcell padding1 width15px">✔</div>
+                <div class="tcell padding1 fweightb">ברקוד:</div>
+                <div class="tcell padding1">Q26B</div>
+           </div>
+            <div class="trow">
+                <div class="tcell padding1 width15px">✔</div>
+                <div class="tcell padding1 fweightb">אפשרות להכניס</div>
+                <div class="tcell padding1">לוגו או חתימה</div>
+           </div>
+            <div class="trow">
+                <div class="tcell padding1 width15px">✔</div>
+                <div class="tcell padding1 fweightb">זמן הכנה:</div>
+                <div class="tcell padding1">במקום עד 3 דקות מרגע 
+				<br>
+				אישור הגהת מלל על ידי הלקוח
+				</div>
+           </div>
+            <div class="trow">
+                <div class="tcell padding1 width15px">✔</div>
+                <div class="tcell padding1 fweightb">החותמת</div>
+                <div class="tcell padding1">עובדת 2 דקות לאחר הכנה</div>
+           </div>
+      </div>
+</body>
+</html>
---
+++ stamp/s160/shulam/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="he" dir="auto">
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>חותמת שולם עם תאריכון Colop s160</title>
+		
+<?php include '../../head/index.php';?>
+
+</head>
+
+<body>
+
+      <div class="table border4px bordergroove borderblack radius25px margin1 padding1 right">
+             <div class="tcaption fweightb center">
+			 <h4 class="fweightb">
+			 פרטי חותמת
+			 </h4>
+			 </div>
+			 <div class="trow">
+                <div class="tcell padding1 width15px">✔</div>
+                <div class="tcell padding1 fweightb">גודל: </div>
+                <div class="tcell padding1">25*12 מ"מ</div>
+             </div>
+            <div class="trow">
+                <div class="tcell padding1 width15px">✔</div>
+                <div class="tcell padding1 fweightb">מלל: </div>
+                <div class="tcell padding1">שולם</div>
+            </div>
+            <div class="trow">
+                <div class="tcell padding1 width15px">✔</div>
+                <div class="tcell padding1 fweightb">צבע דיו:</div>
+                <div class="tcell padding1 blue">כחול</div>
+           </div>
+            <div class="trow">
+                <div class="tcell padding1 width15px">✔</div>
+                <div class="tcell padding1 fweightb">צבע דיו ,תאריך:</div>
+                <div class="tcell padding1 red">אדום</div>
+           </div>
+            <div class="trow">
+                <div class="tcell padding1 width15px">✔</div>
+                <div class="tcell padding1 fweightb">ברקוד:</div>
+                <div class="tcell padding1">9004362302360</div>
+           </div>
+            <div class="trow">
+                <div class="tcell padding1 width15px">✔</div>
+                <div class="tcell padding1 fweightb">זמן הכנה:</div>
+                <div class="tcell padding1">במקום</div>
+           </div>
+      </div>
+</body>
+</html>
